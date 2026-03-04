03c8efa533c4d50d6e550b3ded5f2845ebcafd9b
CeladorLarren <pablocelador@gmail.com>
2026-03-04 17:26:54 +0100

    Arreglo de construcción de config-server, ahora si se guarda en el contenedor el contenido de las configuraciones del resto de servicios.

---
+++ services/servicio-prueba/src/main/java/com/tucanbank/servicio_prueba/ServicioPruebaApplication.java
@@ -0,0 +1,15 @@
+package com.tucanbank.servicio_prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServicioPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioPruebaApplication.class, args);
+	}
+
+}
---
+++ services/servicio-prueba/src/test/java/com/tucanbank/servicio_prueba/ServicioPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tucanbank.servicio_prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
