d78c8f7b997cc23973c09ea013a8dea8951a13b0
celadorlarrenpablo <pabloceladorlarren@gmail.com>
2026-03-11 09:50:13 +0100

    Refactor para EmpleadosApplication

--- services/msempleados/src/main/java/com/tucan/msempleados/EmpleadosApplication.java
+++ services/msempleados/src/main/java/com/tucan/msempleados/EmpleadosApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class RepoEmpleados {
+public class EmpleadosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RepoEmpleados.class, args); 
+		SpringApplication.run(EmpleadosApplication.class, args); 
 	}
 
 }
